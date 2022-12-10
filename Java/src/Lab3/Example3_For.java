afc9e0fbb80ded648f02ce740db467193a14d8ad
Linkazet <linkazet25@gmail.com>
2022-12-10 13:00:48 +0500

    Задача №3 For

--- Java/src/Lab3/Example3_For.java
+++ Java/src/Lab3/Example3_For.java
@@ -8,11 +8,9 @@ public class Example3_For {
         int a = in.nextInt();
         int b = 1; //Первое число последовательности
         int c = 1; //Второе число последовательности
-        System.out.println(b + " ");
-        System.out.println(c + " ");
         for (int i = 1; i<=a; i++) {
+            System.out.println(b + " ");
             int d = b + c;
-            System.out.println(d + " ");
             b = c;
             c = d;
         }
