8e6c812ed03c40f7f814e520897d3a3f1aa3f42f
Linkazet <linkazet25@gmail.com>
2022-12-10 14:55:30 +0500

    Задача №8

---
+++ Java/src/Lab3/Example8.java
@@ -0,0 +1,21 @@
+package Lab3;
+
+public class Example8 {
+    public static void main(String[] args) {
+        char [] a = new char[10];
+        a[0] = 'B';
+        a[1] = 'C';
+        a[2] = 'D';
+        a[3] = 'F';
+        a[4] = 'G';
+        a[5] = 'H';
+        a[6] = 'J';
+        a[7] = 'K';
+        a[8] = 'L';
+        a[9] = 'M';
+        System.out.println("Вывод массива: ");
+        for (int i = 0; i < 9; i++) {
+            System.out.println(a[i] + " ");
+        }
+    }
+}
