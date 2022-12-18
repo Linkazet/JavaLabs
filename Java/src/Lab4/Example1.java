8de85e63eae8a25ddde4d20f38f995d97170baff
Linkazet <linkazet25@gmail.com>
2022-12-18 13:55:04 +0500

    Задача №1

---
+++ Java/src/Lab4/Example1.java
@@ -0,0 +1,20 @@
+package Lab4;
+
+public class Example1 {
+    public static void main(String[] args) {
+        int a = 11; // Число строк, которое необходимо вывести
+        int i;
+        int j;
+        int z;
+
+        for (i = 1; i <= a; i++) {
+            System.out.println("Номер строки: " + i + " ");
+            z = 0;
+            for (j = -11; j <= a; j++) {
+                System.out.println("+");
+                z = z + 1;
+            }
+            System.out.println(" Количество символов в строке " + z);
+        }
+    }
+}
