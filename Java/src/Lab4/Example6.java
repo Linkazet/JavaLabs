c6d3e4337f6bfd7e44d4057dfc69f8238ea88013
Linkazet <linkazet25@gmail.com>
2022-12-18 15:49:05 +0500

    Задача №6

--- Java/src/Lab4/Example6.java
+++ Java/src/Lab4/Example6.java
@@ -13,27 +13,29 @@ public class Example6 {
         int[][] mass1 = new int[a][b];
         Random rand = new Random();
         for (int i = 0; i < a; i++) {
-            System.out.println("Номер строки: " + i + " ");
+            System.out.print("Номер строки: " + i + " | ");
             for (int j = 0; j < b; j++) {
                 mass1[i][j] = rand.nextInt(100);
-                System.out.println(mass1[i][j]);
+                System.out.print(mass1[i][j] + " ");
             }
+            System.out.println();
         }
         int c = rand.nextInt(a - 1);
         int d = rand.nextInt(b - 1);
         int[][] mass2 = new int[a - 1][b - 1];
         for (int i = 0, e = 0; i < a - 1; e++) {
             if (e != c) {
-                System.out.println("Номер строки массива, у которого удалена 1 строка и 1 столбец: " + i + " ");
+                System.out.print("Номер строки массива, у которого удалена 1 строка и 1 столбец: " + i + " | ");
                 for (int j = 0, f = 0; j < b - 1; f++) {
                     if (f != d) {
                         mass2[i][j] = mass1[e][f];
-                        System.out.println(mass2[i][j]);
+                        System.out.print(mass2[i][j] + " ");
                         j++;
                     }
                 }
                 i++;
             }
+            System.out.println();
         }
     }
 }
