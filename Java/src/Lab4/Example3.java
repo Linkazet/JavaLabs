58d40cbde473fdb5c6d9e210cbb81e565142eb60
Linkazet <linkazet25@gmail.com>
2022-12-18 14:10:10 +0500

    Задача №3

---
+++ Java/src/Lab4/Example3.java
@@ -0,0 +1,32 @@
+package Lab4;
+
+import java.util.Scanner;
+
+public class Example3 {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        System.out.println("Введите высоту прямоугольника");
+        int b = in.nextInt();
+        System.out.println("Введите ширину прямоугольника");
+        int c = in.nextInt();
+        int i;
+        int j;
+        int z;
+        int[][] mass = new int [b][c];
+
+        for (i = 0; i < b; i++) {
+            for (j = 0; j < c; j++) {
+                mass[i][j] = 2;
+            }
+        }
+        for (i = 0; i < b; i++) {
+            System.out.println("Номер строки: " + i + " ");
+            z = 0;
+            for (j = 0; j < c; j++) {
+                System.out.println(mass[i][j] + " ");
+                z = z + 1;
+            }
+            System.out.println("Количество символов в строке " + z);
+        }
+    }
+}
